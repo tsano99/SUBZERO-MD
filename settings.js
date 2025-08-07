71bd099f2054e5a0786c98b9bbc806711339db9c
tsano99 <navobot2@gmail.com>
2025-08-07 07:01:57 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;HZEEjRZS#sm4tr8ax8M0_eXIVv2CvdF-P9yRuH8MADEl7zouHaI4", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "263773219130", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
